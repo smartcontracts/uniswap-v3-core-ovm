8bf4a130c7ff79f37c23bdee8ba2e2ce6c8a2ace
Georgios Konstantopoulos <me@gakonst.com>
2021-04-19 12:53:47 +0300

    feat: add ovm compiler

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,34 @@
 import 'hardhat-typechain'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+import '@eth-optimism/hardhat-ovm'
+import 'hardhat-contract-sizer'
+
+import { BigNumber, providers } from 'ethers'
+
+import { extendEnvironment } from "hardhat/config";
+extendEnvironment((hre) => {
+  if (hre.network.name == 'optimism') {
+    // Override Waffle Fixtures to be no-ops, because l2geth does not support
+    // snapshotting
+    // @ts-ignore
+    hre.waffle.loadFixture = async (fixture: Promise<any>) => await fixture()
+
+    // Temporarily set gasPrice = 0, until l2geth provides pre-funded l2 accounts.
+    const provider = new providers.JsonRpcProvider("http://localhost:8545")
+    provider.getGasPrice = async () => BigNumber.from(0)
+    hre.ethers.provider = provider
+  }
+});
 
 export default {
   networks: {
     hardhat: {
       allowUnlimitedContractSize: false,
+    },
+    optimism: {
+      url: "http://localhost:8545",
+      ovm: true,
     },
   },
   solidity: {
